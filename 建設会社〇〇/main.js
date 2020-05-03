1c95c7bedfdd18faff9301609d2c1ec170fef8f9
y-u-t-o <63902764+y-u-t-o@users.noreply.github.com>
2020-05-03 17:56:19 +0900

    Update main.js

--- 建設会社〇〇/main.js
+++ 建設会社〇〇/main.js
@@ -13,12 +13,28 @@ $(function() {
   } else {
     $(function() {
       $('.header-content').show();
-    })
+      $('#second, #third').addClass('is-over');
+    });
   }
 
   $('.header-content a').on('click', function() {
     $('.header-content').fadeToggle('fast');
     $('.header-top_title').toggleClass('show');
     $('.line1, .line2, .line3').toggleClass('open');
+  });
+
+  $(window).on('scroll', function() {
+    $('.service-box').each(function(index,el) {
+      if ( $(window).scrollTop() > ( $(el).offset().top - $(window).height() / 2 ))
+        $(el).addClass('is-over');
+    });
+    $('.project-box').each(function(index,el) {
+      if ( $(window).scrollTop() > ( $(el).offset().top - $(window).height() / 2 ))
+        $(el).addClass('is-over');
+    });
+    $('.btn').each(function(index,el) {
+      if ( $(window).scrollTop() > ( $(el).offset().top - $(window).height() * .8 ))
+        $(el).addClass('is-over');
+    });
   });
 });
